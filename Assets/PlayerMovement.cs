6635e10353625f17e3d7f938db49ec127c3f2b95
skyteks <Schattenpalkia@googlemail.com>
2018-03-07 00:05:20 +0100

    Fixed colliders in castle scene and added walk-to-click functionality

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rigid;
+    private CircleCollider2D coll;
 
     private float x;
     private float y;
@@ -16,9 +18,15 @@ public class PlayerMovement : MonoBehaviour
     private DialogueTrigger dialogueTrigger;
     private DialogueManager dialogueManager;
 
+    private Vector2 destination;
+    private bool gotoDestination;
+
+    public LayerMask layerMask;
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CircleCollider2D>();
         dialogueManager = FindObjectOfType<DialogueManager>();
     }
 
@@ -44,8 +52,30 @@ public class PlayerMovement : MonoBehaviour
             if (dialogueManager.dialogueRunning) return;
         }
 
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            x = Input.GetAxis("Horizontal");
+            y = Input.GetAxis("Vertical");
+            gotoDestination = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 curserPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //RaycastHit2D hit = Physics2D.CircleCast(transform.position, coll.radius, curserPos, Vector2.Distance(transform.position, Input.mousePosition), layerMask);
+            destination = curserPos;//hit.point != Vector2.zero ? curserPos : hit.point;
+            gotoDestination = true;
+        }
+        if (gotoDestination)
+        {
+            Vector3 direction = (Vector3)destination - transform.position;
+            Debug.Log("dir: " + direction);
+            Debug.DrawLine(transform.position, destination, Color.red);
+            Vector2 step = direction.magnitude > 1f ? direction.normalized : direction;
+            x = step.x;
+            y = step.y;
+        }
+        //Collider[] colliders = Physics2D.OverlapCircleAll(transform.position + new Vector3(coll.offset.x, coll.offset.y), coll.radius);
     }
 
     void FixedUpdate()
@@ -58,12 +88,14 @@ public class PlayerMovement : MonoBehaviour
         {
             rigid.AddForce(y * Vector3.up * Time.fixedDeltaTime * speed);
         }
-        x = 0;
-        y = 0;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        gotoDestination = false;
+        x = 0;
+        y = 0;
+
         DialogueTrigger trigger = other.GetComponent<DialogueTrigger>();
         if (trigger != null) dialogueTrigger = trigger;
         //Debug.Log("Entered trigger");
@@ -74,5 +106,23 @@ public class PlayerMovement : MonoBehaviour
         DialogueTrigger trigger = other.GetComponent<DialogueTrigger>();
         if (trigger != null && trigger == dialogueTrigger) dialogueTrigger = null;
         //Debug.Log("Left trigger");
+    }
+
+    private void OnDrawGizmos()
+    {
+        coll = GetComponent<CircleCollider2D>();
+
+        //if (gotoDestination)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(new Vector3(destination.x, destination.y), coll.radius);
+        }
+
+        Vector2 curserPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(curserPos, coll.radius);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position + new Vector3(x, y), coll.radius);
     }
 }
